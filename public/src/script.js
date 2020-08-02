9eacf6a11176e0478468144d31864aebdd09a9b2
e-pena <63423138+e-pena@users.noreply.github.com>
2020-08-02 03:25:47 -0300

    Se agregaron fc para reservar paquetes

--- public/src/script.js
+++ public/src/script.js
@@ -15,6 +15,38 @@ const formularioLogIn = document.querySelector('#form-login');
 const formularioSignIn = document.querySelector('#form-signin');
 const saludoUsuario = document.querySelector('#saludo-usuario');
 const logInFallido = document.querySelector('#login-fallido');
+const btnReserva = document.querySelectorAll('.btn-success');
+const btnCompra = document.querySelectorAll('.btn-danger');
+const paqueteLugar = document.querySelectorAll('.lugar');
+const paquetePrecio = document.querySelectorAll('.precio');
+const paqueteFecha = document.querySelectorAll('.fecha');
+const paqueteImagen = document.querySelectorAll('.foto');
+
+let idUsuarioActual;
+
+function cargarPaquetes() {
+	try {
+		const response = fetch('http://127.0.0.1:3000/paquetes/')
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				for (let i = 0; i < data.length; i++) {
+					const element = data[i];
+					paqueteLugar[i].innerText = element.destino;
+					paquetePrecio[i].innerText = `Precio: ${element.precio}`;
+					paqueteFecha[i].innerText = `Fechas: ${element.fecha}`;
+					paqueteImagen[i].setAttribute('src', element.imagen);
+					paqueteImagen[i].setAttribute('alt', element.destino);
+				}
+				return data;
+			});
+	} catch (error) {
+		return console.error(error);
+	}
+}
+
+cargarPaquetes();
 
 function logIn(usuario) {
 	try {
@@ -34,6 +66,7 @@ function logIn(usuario) {
 						saludoUsuario.innerText = `Bienvenido de nuevo ${usuario.nombre} ${usuario.apellido}`;
 						saludoUsuario.classList.remove('oculto');
 						logInFallido.classList.add('oculto');
+						idUsuarioActual = element.id;
 						return data;
 					}
 				}
@@ -59,6 +92,7 @@ function signIn(usuario) {
 			})
 			.then((data) => {
 				console.log(data);
+				idUsuarioActual = data.id;
 			});
 	} catch (error) {
 		return error;
@@ -109,3 +143,52 @@ postBtn.addEventListener('click', (e) => {
 	saludoUsuario.classList.remove('oculto');
 	logInFallido.classList.add('oculto');
 });
+
+function reconocerPaquete(ubicacionPaquete) {
+	try {
+		const response = fetch('http://127.0.0.1:3000/paquetes/')
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				let paquete = data[ubicacionPaquete];
+				let paqueteReservado = {
+					idUsuario: idUsuarioActual,
+					idPaquete: paquete.id,
+					pagoRealizado: false,
+					precio: paquete.precio,
+				};
+				reservarPaquete(paqueteReservado);
+				return data;
+			});
+		return response;
+	} catch (error) {
+		return error;
+	}
+}
+
+function reservarPaquete(paqueteReservado) {
+	try {
+		fetch('http://127.0.0.1:3000/compras', {
+			method: 'POST',
+			body: JSON.stringify(paqueteReservado),
+			headers: { 'Content-Type': 'application/json' },
+		})
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				return data;
+			});
+	} catch (error) {
+		return error;
+	}
+}
+
+for (let i = 0; i < btnReserva.length; i++) {
+	const element = btnReserva[i];
+	element.addEventListener('click', () => {
+		let paquete = reconocerPaquete(i);
+		return paquete;
+	});
+}
