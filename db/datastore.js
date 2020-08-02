bc5dfa3f04ac09df1eeaf0fd82fb22cbb3ce4bea
e-pena <63423138+e-pena@users.noreply.github.com>
2020-08-02 03:24:25 -0300

    Agregué imagen y fc para agregar compra

--- db/datastore.js
+++ db/datastore.js
@@ -4,6 +4,7 @@ let paquetes = [
 		destino: 'Cataratas del Iguazú',
 		precio: 5000,
 		fecha: '10-08-2021 a 20-08-2021',
+		imagen: 'https://miequipajedemano.com/wp-content/uploads/2018/01/visitar-las-cataratas-del-iguazu.jpg',
 		compradores: [
 			{
 				idDeComprador: 1,
@@ -20,6 +21,7 @@ let paquetes = [
 		destino: 'Cusco',
 		precio: 5000,
 		fecha: '10-08-2021 a 20-08-2021',
+		imagen: 'https://www.aviatur.com/source/revista-horizontes/machu-picchu-peru.jpg',
 		compradores: [],
 	},
 	{
@@ -27,6 +29,7 @@ let paquetes = [
 		destino: 'Japón',
 		precio: 5000,
 		fecha: '10-08-2021 a 20-08-2021',
+		imagen: 'https://mochilerosentailandia.com/wp-content/uploads/2018/07/japon-primavera.jpg',
 		compradores: [],
 	},
 	{
@@ -34,6 +37,8 @@ let paquetes = [
 		destino: 'New York',
 		precio: 5000,
 		fecha: '10-08-2021 a 20-08-2021',
+		imagen:
+			'https://s3-eu-west-1.amazonaws.com/eflanguagesblog/wp-content/uploads/sites/26/2018/07/18150652/new-york-movies.jpg',
 		compradores: [
 			{
 				idDeComprador: 3,
@@ -50,6 +55,8 @@ let paquetes = [
 		destino: 'París',
 		precio: 5000,
 		fecha: '10-08-2021 a 20-08-2021',
+		imagen:
+			'https://86087-590828-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2016/01/paris-fin-de-semana-1024x683.jpeg',
 		compradores: [],
 	},
 	{
@@ -57,6 +64,8 @@ let paquetes = [
 		destino: 'Punta del Este',
 		precio: 5000,
 		fecha: '10-08-2021 a 20-08-2021',
+		imagen:
+			'https://s1.eestatic.com/2019/12/01/viajes/Viajes-Punta_del_Este-Uruguay-Viajes_448717341_139407134_1706x960.jpg',
 		compradores: [],
 	},
 	{
@@ -64,6 +73,7 @@ let paquetes = [
 		destino: 'Río de Janeiro',
 		precio: 5000,
 		fecha: '10-08-2021 a 20-08-2021',
+		imagen: 'https://www.turismoviajar.com/wp-content/uploads/2019/10/Rio-de-Janeiro-2020.jpg',
 		compradores: [],
 	},
 	{
@@ -71,6 +81,7 @@ let paquetes = [
 		destino: 'Roma',
 		precio: 5000,
 		fecha: '10-08-2021 a 20-08-2021',
+		imagen: 'https://static.ellitoral.com/um/fotos/297292_07.jpg',
 		compradores: [],
 	},
 	{
@@ -78,6 +89,8 @@ let paquetes = [
 		destino: 'Ushuaia',
 		precio: 5000,
 		fecha: '10-08-2021 a 20-08-2021',
+		imagen:
+			'https://www.cronista.com/__export/1503626112241/sites/diarioelcronista/img/2017/08/24/3d4.jpg_258117318.jpg',
 		compradores: [],
 	},
 ];
@@ -240,4 +253,19 @@ function agregarUsuario(usuario) {
 	return nuevoUsuario;
 }
 
-module.exports = { paquetes, usuarios, usuarioPaqueteComprado, crearNuevoPaquete, agregarUsuario };
+function agregarCompra(compra) {
+	let nuevaCompra = compra;
+	nuevaCompra.id = obtenerUltimoIdUsuarioPaqueteComprado();
+	usuarioPaqueteComprado.push(nuevaCompra);
+	let nuevaCompraPaquete = paquetes.find((r) => r.id == nuevaCompra.idPaquete);
+	let objetoPaquete = {
+		idDeComprador: nuevaCompra.idUsuario,
+		pagoRealizado: nuevaCompra.pagoRealizado,
+	};
+	nuevaCompraPaquete.compradores.push(objetoPaquete);
+	let nuevaCompraUsuario = usuarios.find((r) => r.id == nuevaCompra.idUsuario);
+	nuevaCompraUsuario.idDePaquetesComprados.push(nuevaCompra.idPaquete);
+	return nuevaCompra;
+}
+
+module.exports = { paquetes, usuarios, usuarioPaqueteComprado, crearNuevoPaquete, agregarUsuario, agregarCompra };
